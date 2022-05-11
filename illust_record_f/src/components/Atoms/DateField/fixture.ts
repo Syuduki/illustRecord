130dd0e9a13bc3f8ff54e2c71848bed91235effb
Syuduki <yudumashi@gmail.com>
2022-05-12 00:00:30 +0900

    add : テストコード追加、モックデータ追加

---
+++ illust_record_f/src/components/Atoms/DateField/fixture.ts
@@ -0,0 +1,10 @@
+import * as types from './types';
+
+export const props: types.Props = {
+  id: 'dateField',
+  value: '2022/02/02',
+  setValue: jest.fn(),
+  size: 'small',
+  variant: 'standard',
+  label: 'MOCK日付',
+};
