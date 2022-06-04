961bc7bff745320e240ce2c6356fecdd1111f56b
Syuduki <yudumashi@gmail.com>
2022-06-04 23:22:56 +0900

    Update : 新規コンポーネント実装

--- illust_record_f/src/components/Organisms/TagList/TagList.tsx
+++ illust_record_f/src/components/Organisms/TagList/TagList.tsx
@@ -1,19 +1,16 @@
-import * as React from 'react';
-import { Props } from './types';
-import { SearchArea } from '../../Molecules/SearchArea';
+import * as React from "react";
+import { Props } from "./types";
 
-import Tabs from '@mui/material/Tabs';
-import Tab from '@mui/material/Tab';
-import Box from '@mui/material/Box';
-import Stack from '@mui/material/Stack';
-import Button from '@mui/material/Button';
+import Box from "@mui/material/Box";
+import Stack from "@mui/material/Stack";
+import Button from "@mui/material/Button";
+import { IndexSearch } from "../../Molecules/IndexSearch";
+import { SearchField } from "../../Atoms/SearchField";
 
 export const TagList: React.FC<Props> = ({ ...props }) => {
-  const [value, setValue] = React.useState('あ');
-  const handleChange = (event: React.SyntheticEvent, newValue: string) => {
-    setValue(newValue);
-    props.onSearch('tab', newValue);
-  };
+  const [index, setIndex] = React.useState<
+    "あ" | "か" | "さ" | "た" | "な" | "は" | "ま" | "や" | "ら" | "わ" | ""
+  >("あ");
 
   const selectTag = (id: number) => {
     let tempSelectedTags = [...props.selectedTags];
@@ -26,49 +23,58 @@ export const TagList: React.FC<Props> = ({ ...props }) => {
   };
 
   return (
-    <Box sx={{ width: '100%' }}>
-      <SearchArea />
-      <Tabs value={value} onChange={handleChange}>
-        {['あ', 'か', 'さ', 'た', 'な', 'は', 'ま', 'や', 'ら', 'わ'].map(
-          (value) => (
-            <Tab key={value} value={value} label={value} />
-          )
-        )}
-      </Tabs>
+    <Box sx={{ width: "100%" }}>
       <Stack
-        direction="row"
-        justifyContent="flex-start"
-        alignItems="flex-start"
-        spacing={6}
-        style={{ marginTop: '20px' }}
+        direction="column"
+        justifyContent="center"
+        alignItems="center"
+        spacing={2}
       >
-        {props.tagList.map((tag) => (
-          <>
-            {props.type === 'select' && (
-              <Button
-                key={tag.id}
-                variant={
-                  props.selectedTags?.includes(tag.id)
-                    ? 'contained'
-                    : 'outlined'
-                }
-                onClick={() => selectTag(tag.id)}
-              >
-                {tag.name}
-              </Button>
-            )}
-            {props.type === 'register' && (
-              <Button
-                key={tag.id}
-                variant="text"
-                onClick={() => props.onClickTag(tag.id)}
-                size="large"
-              >
-                {tag.name}
-              </Button>
-            )}
-          </>
-        ))}
+        <SearchField
+          placeholder="タグを検索"
+          onSubmit={(keyword) => props.onSearch("input", keyword)}
+          width={400}
+        />
+        <IndexSearch
+          index={index}
+          setIndex={setIndex}
+          onChange={(keyword) => props.onSearch("tab", keyword)}
+        />
+        <Stack
+          direction="row"
+          justifyContent="flex-start"
+          alignItems="flex-start"
+          spacing={6}
+          style={{ marginTop: "20px" }}
+        >
+          {props.tagList.map((tag) => (
+            <>
+              {props.type === "select" && (
+                <Button
+                  key={tag.id}
+                  variant={
+                    props.selectedTags?.includes(tag.id)
+                      ? "contained"
+                      : "outlined"
+                  }
+                  onClick={() => selectTag(tag.id)}
+                >
+                  {tag.name}
+                </Button>
+              )}
+              {props.type === "register" && (
+                <Button
+                  key={tag.id}
+                  variant="text"
+                  onClick={() => props.onClickTag(tag.id)}
+                  size="large"
+                >
+                  {tag.name}
+                </Button>
+              )}
+            </>
+          ))}
+        </Stack>
       </Stack>
     </Box>
   );
