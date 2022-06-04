fdb5d109a531631c70adb8ef201d0932c7237760
Syuduki <yudumashi@gmail.com>
2022-06-04 23:56:12 +0900

    Update : 検索時索引の状態を空文字にする

--- illust_record_f/src/components/Organisms/TagList/TagList.tsx
+++ illust_record_f/src/components/Organisms/TagList/TagList.tsx
@@ -1,16 +1,16 @@
-import * as React from "react";
-import { Props } from "./types";
+import * as React from 'react';
+import { Props } from './types';
 
-import Box from "@mui/material/Box";
-import Stack from "@mui/material/Stack";
-import Button from "@mui/material/Button";
-import { IndexSearch } from "../../Molecules/IndexSearch";
-import { SearchField } from "../../Atoms/SearchField";
+import Box from '@mui/material/Box';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+import { IndexSearch } from '../../Molecules/IndexSearch';
+import { SearchField } from '../../Atoms/SearchField';
 
 export const TagList: React.FC<Props> = ({ ...props }) => {
   const [index, setIndex] = React.useState<
-    "あ" | "か" | "さ" | "た" | "な" | "は" | "ま" | "や" | "ら" | "わ" | ""
-  >("あ");
+    'あ' | 'か' | 'さ' | 'た' | 'な' | 'は' | 'ま' | 'や' | 'ら' | 'わ' | ''
+  >('あ');
 
   const selectTag = (id: number) => {
     let tempSelectedTags = [...props.selectedTags];
@@ -23,7 +23,7 @@ export const TagList: React.FC<Props> = ({ ...props }) => {
   };
 
   return (
-    <Box sx={{ width: "100%" }}>
+    <Box sx={{ width: '100%' }}>
       <Stack
         direction="column"
         justifyContent="center"
@@ -32,37 +32,40 @@ export const TagList: React.FC<Props> = ({ ...props }) => {
       >
         <SearchField
           placeholder="タグを検索"
-          onSubmit={(keyword) => props.onSearch("input", keyword)}
+          onSubmit={(keyword) => {
+            setIndex('');
+            props.onSearch('input', keyword);
+          }}
           width={400}
         />
         <IndexSearch
           index={index}
           setIndex={setIndex}
-          onChange={(keyword) => props.onSearch("tab", keyword)}
+          onChange={(keyword) => props.onSearch('tab', keyword)}
         />
         <Stack
           direction="row"
           justifyContent="flex-start"
           alignItems="flex-start"
           spacing={6}
-          style={{ marginTop: "20px" }}
+          style={{ marginTop: '20px' }}
         >
           {props.tagList.map((tag) => (
             <>
-              {props.type === "select" && (
+              {props.type === 'select' && (
                 <Button
                   key={tag.id}
                   variant={
                     props.selectedTags?.includes(tag.id)
-                      ? "contained"
-                      : "outlined"
+                      ? 'contained'
+                      : 'outlined'
                   }
                   onClick={() => selectTag(tag.id)}
                 >
                   {tag.name}
                 </Button>
               )}
-              {props.type === "register" && (
+              {props.type === 'register' && (
                 <Button
                   key={tag.id}
                   variant="text"
